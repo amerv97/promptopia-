01ef3220d54fd34e1b2e1dcf8bdfa25d5bd78189
Amer Varajic <varajicamer@gmail.com>
2023-05-08 16:16:05 +0200

    imported Nav & Feed on page and layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,15 @@
 import "@styles/globals.css";
 import { Children } from "react";
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
@@ -15,7 +18,8 @@ const RootLayout = ({children}) => {
         </div>
 
         <main className="app">
-            {children}
+          <Nav />
+          {children}
         </main>
       </body>
     </html>

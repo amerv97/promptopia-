3263504d1f24b55e93ee03a9aaab5a702ab3b62f
Amer Varajic <varajicamer@gmail.com>
2023-05-11 12:11:06 +0200

    Finished route for create prompt & added useRouter

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,9 +1,23 @@
 import { connectionToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
 export const POST = async (req) => {
-  const { userId, promp, tag } = await req.json();
+  const { userId, prompt, tag } = await req.json();
 
   try {
     await connectionToDB();
-  } catch (error) {}
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), {
+      status: 201,
+    });
+  } catch (error) {
+    return new Response("Failed to create a new prompt", { status: 500 });
+  }
 };
